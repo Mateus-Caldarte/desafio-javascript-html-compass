9f5c458498e381fa062b05e668233a99707878f8
Mateus Caldarte <mateuscaldarte@hotmail.com>
2021-12-29 16:00:01 -0300

    corrigindo bug de conexão com api do Léo

--- jsFormulario.js
+++ jsFormulario.js
@@ -6,15 +6,31 @@ function validar(){
          erro.classList.toggle('d-none');
          
     }
+    else{
+        enviar();
+    }
 }
 function enviar(){
-    var userName = document.getElementById('nome').value;
-    $.ajax({
-    type: "POST",
-    url: `https://prog-bolsas-api.herokuapp.com/api/${userName}`,
-    data: data,
-    success: function(data){ 
-        console.log(data);
-    },
+    let userName = document.getElementById('nome').value;
+    let userEmail = document.getElementById('email').value;
+    let userMsg = document.getElementById('mensagem').value;
+    var url = "https://prog-bolsas-api.herokuapp.com/";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    
+    xhr.onreadystatechange = function () {
+       if (xhr.readyState === 4) {
+          console.log(xhr.status);
+          console.log(xhr.responseText);
+       }};
+    let data = JSON.stringify({
+        "nome": `${userName}`,
+        "email": `${userEmail}`,
+        "msg": `${userMsg}`,
     });
+    xhr.send(data);
 }
